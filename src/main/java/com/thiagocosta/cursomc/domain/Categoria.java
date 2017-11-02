1dbba55089e4c32625d47faad3f5df3f0f7dec90
Thiago Costa <thiago_cost@hotmail.com>
2017-11-02 11:02:43 -0300

    Testando a primeira classe de dominio - Categoria

---
+++ src/main/java/com/thiagocosta/cursomc/domain/Categoria.java
@@ -0,0 +1,63 @@
+package com.thiagocosta.cursomc.domain;
+
+import java.io.Serializable;
+
+public class Categoria implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Integer idCategoria;
+	private String nome;
+	
+	
+	public Categoria(Integer idCategoria, String nome) {
+		super();
+		this.idCategoria = idCategoria;
+		this.nome = nome;
+	}
+
+	public Integer getIdCategoria() {
+		return idCategoria;
+	}
+
+	public void setIdCategoria(Integer idCategoria) {
+		this.idCategoria = idCategoria;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((idCategoria == null) ? 0 : idCategoria.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (idCategoria == null) {
+			if (other.idCategoria != null)
+				return false;
+		} else if (!idCategoria.equals(other.idCategoria))
+			return false;
+		return true;
+	}
+	
+	
+}
--- src/main/java/com/thiagocosta/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/thiagocosta/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,32 @@
 package com.thiagocosta.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.thiagocosta.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1,"Informatica");
+		Categoria cat2 = new Categoria(2,"Escritorio");
+		
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+	    return lista;
+		
+		
 
 	}
 
